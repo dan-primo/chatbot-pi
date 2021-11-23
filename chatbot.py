ddf2491fecd14d8878fb61568671b46d08705866
Danielly Primo <60229135+dan-primo@users.noreply.github.com>
2021-11-23 00:30:59 -0300

    refatoracao-versao-final

--- chatbot.py
+++ chatbot.py
@@ -3,24 +3,24 @@ from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 # Creating ChatBot Instance
-chatbot = ChatBot('ChatBot-PI')
+chatbot = ChatBot('ChatbotPI')
 
 # Training with Personal Ques & Ans
 conversation = [
-    "Oi",
-    "Olá!",
-    "Como vai?",
-    "Estou bem.",
-    "Que bom!",
-    "Obrigado.",
-    "De nada!"
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
 ]
 
 trainer = ListTrainer(chatbot)
 trainer.train(conversation)
 
-# Training with Portuguese Corpus Data
+# Training with English Corpus Data
 trainer_corpus = ChatterBotCorpusTrainer(chatbot)
 trainer_corpus.train(
-    'chatterbot.corpus.portuguese'
+    'chatterbot.corpus.english'
 )
